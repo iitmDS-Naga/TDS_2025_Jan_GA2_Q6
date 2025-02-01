5cc88e7a0e4311d43b550853f17e3a3d6c532f64
nagad4114 <23f3004114@ds.study.iitm.ac.in>
2025-02-01 16:25:28 +0530

    add cors

--- api/main.py
+++ api/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI, Query, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 import json
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 # Load data from JSON file
 with open("q-vercel-python.json", "r") as f:
