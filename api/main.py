d61633271797b1225f7d1111f3e01e030cb9cbf7
nagad4114 <23f3004114@ds.study.iitm.ac.in>
2025-02-01 18:25:06 +0530

    remove mistake code

--- api/main.py
+++ api/main.py
@@ -26,35 +26,7 @@ async def get_students(class_: list[str] = Query(None, alias="class")):
     if class_:
         filtered_students = [student for student in students_data if student["class"] in class_]
         return filtered_students
-    return students_data    from fastapi import FastAPI, Query
-    from fastapi.middleware.cors import CORSMiddleware
-    import pandas as pd
-    
-    app = FastAPI()
-    
-    # Enable CORS
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=["*"],  # Allows all origins
-        allow_credentials=True,
-        allow_methods=["*"],  # Allows all methods
-        allow_headers=["*"],  # Allows all headers
-    )
-    
-    # Load data from CSV file
-    students_df = pd.read_csv("q-fastapi.csv")
-    students_data = students_df.to_dict(orient="records")
-    
-    @app.get("/api")
-    async def get_students(class_: list[str] = Query(None, alias="class")):
-        if class_:
-            filtered_students = [student for student in students_data if student["class"] in class_]
-            return filtered_students
-        return students_data
-    
-    @app.get("/")
-    async def read_root():
-        return {"Hello": "World"}
+    return students_data
 
 # @app.get("/api")
 # async def get_marks(name: list[str] = Query(..., description="List of name to get marks for")):
